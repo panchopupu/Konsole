00a011ed0aaf4eff9f7d42a2deb0312d3d72c8b7
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2019-01-10 02:50:35 +0800

    fix: comp cmd def now calls catch all even if no params
    resolves #10

---
+++ tests/P3.Konsole.Tests/Assets/AutoNDataAttribute.cs
@@ -0,0 +1,24 @@
+﻿using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using AutoFixture.Xunit2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P3.Konsole.Tests.Assets
+{
+    public class AutoNDataAttribute : AutoDataAttribute
+    {
+        public AutoNDataAttribute()
+            : base(() => new Fixture()
+                .Customize(new AutoNSubstituteCustomization() {
+                    ConfigureMembers = true
+                })) {
+
+        }
+        protected AutoNDataAttribute(IFixture fixture)
+            : base(() => fixture.Customize(new AutoNSubstituteCustomization())) {
+
+        }
+    }
+}
---
+++ tests/P3.Konsole.Tests/Assets/InlineAutoNDataAttribute.cs
@@ -0,0 +1,11 @@
+﻿using AutoFixture.Xunit2;
+
+namespace P3.Konsole.Tests.Assets
+{
+    public class InlineAutoNDataAttribute : InlineAutoDataAttribute
+    {
+        public InlineAutoNDataAttribute(params object[] values)
+            : base(new AutoNDataAttribute(), values) {
+        }
+    }
+}
